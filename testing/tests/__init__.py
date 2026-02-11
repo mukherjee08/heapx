2650948cccd7194f561a81f566ab9adb945db197
Ivan Margiela <aniruddhamukherjee2000@gmail.com>
2026-02-10 22:48:55 -0800

    rm -rf testing
    rm -rf testing

--- testing/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-'''
-This file marks the `tests/` directory as a Python package. 
-While intentionally empty of executable code, its presence is 
-architecturally essential for the heapx modle's testing infrastructure.
-'''
